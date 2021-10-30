518c0738c2d1010ec89c650f074984b4af6000b1
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-10-30 02:11:11 +0100

    updated js functionality

---
+++ js/appointments.js
@@ -0,0 +1,74 @@
+const consultant = JSON.parse(localStorage.getItem('Consultant'))
+
+async function getAppointment() {
+    let url = 'https://skheduler.herokuapp.com/api/appointments'
+
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+
+        const appointments = data.filter((item) => item.user.userId === consultant._id)
+
+        if (appointments) {
+            let output = ''
+            appointments.forEach(function(user) {
+                output += `
+
+                <div class="col-md-4" >
+                    <div class="box">
+                        <div class="our-services settings text-left" >
+                            <ul><br>
+                                <li> Date: ${user.availableTimes[0].day} </li>
+                                <li> Title: ${user.title} </li>
+                                <li> Name: ${user.user.name} </li>
+                                <li> Address: ${user.venue.address} </li>
+                                <li> Type: ${user.venue.type} </li>
+                                <li> user Id: ${user.user.userId} </li> 
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+               `
+            })
+            document.getElementById('date').innerHTML = output
+        }
+
+    } catch(err) {
+        error.innerText = err.message
+    } 
+
+
+
+    // fetch(url)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data)
+    //         let output = '';
+    //         data.forEach(function(user) {
+    //             console.log(user)
+    //             output += `
+
+    //             <div class="col-md-4" >
+    //                 <div class="box">
+    //                     <div class="our-services settings text-left" >
+    //                         <ul><br>
+    //                             <li> Date: ${user.availableTimes[0].day} </li>
+    //                             <li> Title: ${user.title} </li>
+    //                             <li> Name: ${user.user.name} </li>
+    //                             <li> Address: ${user.venue.address} </li>
+    //                             <li> Type: ${user.venue.type} </li>
+    //                             <li> user Id: ${user.user.userId} </li> 
+    //                         </ul>
+    //                     </div>
+    //                 </div>
+    //             </div>
+                
+    //            `;
+    //         });
+    //         document.getElementById('date').innerHTML = output;
+    //     })
+}
+
+
+getAppointment()
--- js/login.js
+++ js/login.js
@@ -1,25 +1,42 @@
-let form = document.getElementById('form');
+let form = document.getElementById('form')
+const error = document.getElementById('error')
+
 form.addEventListener('submit', function(e) {
     e.preventDefault()
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    console.log(email, password)
-    let postUrl = 'https://skheduler.herokuapp.com/api/user/login';
-    fetch(postUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        })
-        .then(function(response) {
-            location.replace("http://127.0.0.1:5501/consultant/home.html");
-            console.log(response);
-        })
-        .catch(function(error) {
-            console.log('Request failed', error)
-        })
-});
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    if (!(email && password)) {
+        error.innerText = 'All fields are required'
+        console.log(email, password)
+    } else {
+        
+        let postUrl = 'https://skheduler.herokuapp.com/api/user/login'
+
+        const loginUser = async () => {
+            try {
+                const response = await fetch(postUrl, {
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password
+                    })
+                })
+
+                const data = await response.json()
+            
+                if (data) {
+                    location.href = "http://127.0.0.1:5501/consultant/home.html"
+                }
+            } catch(error) {
+                console.log(error)
+            } 
+        }
+        
+        loginUser()
+    }
+})
