4147eb7a03836cb11c7fd59efe9cc9e7b2c7721c
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-10-30 02:42:49 +0100

    removed comment

--- js/appointments.js
+++ js/appointments.js
@@ -40,37 +40,6 @@ async function getAppointment() {
     } catch(err) {
         error.innerText = err.message
     } 
-
-
-
-    // fetch(url)
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //         console.log(data)
-    //         let output = '';
-    //         data.forEach(function(user) {
-    //             console.log(user)
-    //             output += `
-
-    //             <div class="col-md-4" >
-    //                 <div class="box">
-    //                     <div class="our-services settings text-left" >
-    //                         <ul><br>
-    //                             <li> Date: ${user.availableTimes[0].day} </li>
-    //                             <li> Title: ${user.title} </li>
-    //                             <li> Name: ${user.user.name} </li>
-    //                             <li> Address: ${user.venue.address} </li>
-    //                             <li> Type: ${user.venue.type} </li>
-    //                             <li> user Id: ${user.user.userId} </li> 
-    //                         </ul>
-    //                     </div>
-    //                 </div>
-    //             </div>
-                
-    //            `;
-    //         });
-    //         document.getElementById('date').innerHTML = output;
-    //     })
 }
 
 
